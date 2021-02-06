2465ae1138c58b45253d184044af50e491b099b2
Iurii Tatishchev <itatishch@gmail.com>
2021-02-05 22:39:14 -0800

    add the rest of the chat data to chat store state

--- module/src/store/modules/chat/state.ts
+++ module/src/store/modules/chat/state.ts
@@ -1,9 +1,16 @@
-import { StoreMeta } from "../../../@types";
+import { ChatUser, Room, StoreMeta } from "../../../@types";
+import { ObjectId } from "bson";
 
 const state: {
   meta?: StoreMeta.Meta;
+  currentRoom?: ObjectId;
+  rooms?: Array<Room>;
+  users?: Array<ChatUser>;
 } = {
   meta: undefined,
+  currentRoom: undefined,
+  rooms: undefined,
+  users: undefined,
 };
 
 export default state;
